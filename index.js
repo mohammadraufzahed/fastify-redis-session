0e9ff1d073bcb19f03dc53ddfae5bf2fcde54cff
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2023-03-25 15:26:59 +0330

    Revert "Initialize the plugin"
    This reverts commit b70350a4a90dd9ca9839fd288cb8042965ac46b9.

--- index.js
+++
@@ -1,8 +0,0 @@
-const fastifyPlugin = require("fastify-plugin");
-
-function fastifyRedisSession(fastify, options, next) {}
-
-module.exports = fastifyPlugin(fastifyRedisSession, {
-  fastify: "4.x",
-  name: "@fastify/redis-session",
-});
